3a647fd69d1faf2bc3f46164be5e68f72d062fb6
MaryAhn <khahn196@gmail.com>
2021-10-12 23:23:56 +0900

    Create DFS_binary_array.py
    DFS at binary tree implemented by array (stack)

---
+++ Graph/DFS_binary_array.py
@@ -0,0 +1,31 @@
+n = int(input())
+graph = list(map(int, input().split()))
+stack = [0]
+visited = [-1 for _ in range(n)]
+res = []
+
+while len(res) != len(graph):
+    print(stack, res)
+    present = stack.pop()
+    left = 2 * present + 1
+    right = 2 * present + 2
+
+    if visited[present] == -1:
+        res.append(graph[present])
+        stack.append(present)
+        visited[present] += 1
+
+    if left < n or right < n:
+        if visited[left] == -1 and left < n:
+            stack.append(left)
+        elif visited[right] == -1 and right < n:
+            stack.append(right)
+
+print(res)
+
+
+
+
+
+
+
