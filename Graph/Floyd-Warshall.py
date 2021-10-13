3971011f4124ce83de1f1adc6508659ce852e514
MaryAhn <khahn196@gmail.com>
2021-10-13 09:27:25 +0900

    Update Floyd-Warshall.py

--- Graph/Floyd-Warshall.py
+++ Graph/Floyd-Warshall.py
@@ -14,9 +14,9 @@ for i in range(n):
         else:
             dist[i][j] = float('inf')
 
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
             if dist[i][j] > dist[i][k] + dist[k][j]:
                 dist[i][j] = dist[i][k] + dist[k][j]
 
